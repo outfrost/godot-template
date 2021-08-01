327627b8ed539364e2fe459a469a3bf25b66f1d4
outfrost <kotlet.bahn@gmail.com>
2021-08-01 17:05:16 +0200

    Convert DebugLabel into singleton overlay

--- game/Game.gd
+++ game/Game.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	main_menu.connect("start_game", self, "on_start_game")
 
 func _process(delta: float) -> void:
-	DebugLabel.display(self, "fps %d" % Performance.get_monitor(Performance.TIME_FPS))
+	DebugOverlay.display("fps %d" % Performance.get_monitor(Performance.TIME_FPS))
 
 	if Input.is_action_just_pressed("menu"):
 		back_to_menu()
