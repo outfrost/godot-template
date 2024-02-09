a9df37bc94b9792db6e2f9380754515974e5bc78
outfrost <kotlet.bahn@gmail.com>
2024-02-09 01:23:51 +0100

    Automate version text

---
+++ game/menu/VersionLabel.gd
@@ -0,0 +1,15 @@
+class_name VersionLabel
+extends Label
+
+const VERSION_PATH: = "res://version.tres"
+
+func _ready() -> void:
+	var v = FileAccess.get_file_as_string(VERSION_PATH)
+	if FileAccess.get_open_error() != OK:
+		push_error("cannot open version file")
+		text = ""
+	else:
+		text = v.rstrip("\n")
+
+	if OS.has_feature("debug"):
+		text += " (DEBUG)"
