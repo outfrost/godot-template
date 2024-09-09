9334175465f709d6971f8e43e3adeeb76bb5fe03
outfrost <kotlet.bahn@gmail.com>
2024-09-09 05:27:45 +0200

    Update Harbinger

--- game/Harbinger.gd
+++ game/Harbinger.gd
@@ -3,15 +3,29 @@ extends Node
 var subs: Dictionary = {}
 
 func subscribe(msg: String, receiver: Callable) -> void:
-	var rx_list: Array[Callable] = subs.get(msg, [])
+	if !subs.has(msg):
+		subs[msg] = [] as Array[Callable]
+	var rx_list: Array[Callable] = subs[msg]
 	rx_list.append(receiver)
 
-func dispatch(msg: String) -> void:
-	var rx_list: Array[Callable] = subs.get(msg, [])
+func dispatch(msg: String, params: Array = []) -> void:
+	if !subs.has(msg):
+		subs[msg] = [] as Array[Callable]
+	var rx_list: Array[Callable] = subs[msg]
 	for rx in rx_list:
-		rx.call()
+		if rx.is_valid():
+			rx.call(params)
 
-func dispatch_deferred(msg: String) -> void:
-	var rx_list: Array[Callable] = subs.get(msg, [])
+func dispatch_deferred(msg: String, params: Array = []) -> void:
+	if !subs.has(msg):
+		subs[msg] = [] as Array[Callable]
+	var rx_list: Array[Callable] = subs[msg]
 	for rx in rx_list:
-		rx.call_deferred()
+		if rx.is_valid():
+			rx.call_deferred(params)
+
+func prune() -> void:
+	for msg in subs:
+		var rx_list: Array[Callable] = subs[msg] as Array[Callable]
+		rx_list = rx_list.filter(func(rx): return rx.is_valid())
+		subs[msg] = rx_list
